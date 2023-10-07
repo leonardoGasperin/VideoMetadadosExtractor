59898c97123f72318cefb61ebf755b4b6940ea2a
leonardoGasperin <leonardovdg@hotmail.com>
2023-10-07 16:43:00 -0300

    criando entidade para armazenamento de dados extraidos

---
+++ VideoMetadadosExtractor/Entities/VideoMetadata.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities;
+
+public class VideoMetadata
+{
+    public long DurationInSeconds { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public string? FrameRate { get; set; }
+}
